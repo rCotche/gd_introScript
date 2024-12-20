9544fc83c607ef5aa0a9201e9033a13f4144e67c
rCotche <roshan.cotche@gmail.com>
2024-12-20 21:37:06 +0400

    functions

--- main.gd
+++ main.gd
@@ -1,12 +1,22 @@
 extends Node
 
-#var damage: int = 15 #la var "damage" de type int
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("my_action"):
+		#jump()
 #
-#@export va nous permettre de l'utiliser dans l'inspector
-#@export var damage := 15 #godot will determine automatically le type de la var
-#
-#const var that cannot change
-#bonne pratique de mttre les const en capital
-const GRAVITY = -9.81
+#func jump():
+	##add upward force
+	##play a boing song
+	##play jump animation
+	#print("jump")
+
 func _ready() -> void:
-	pass
+	var result = add(3,5)
+	print(result)
+
+#typé les params = num1: int
+# return type syntaxe = -> int
+func add(num1: int, num2: int) -> int:
+	var result = num1 + num2
+	#print(result)
+	return result
