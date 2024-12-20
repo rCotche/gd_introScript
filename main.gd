56b1370636e22dcd54516162f493197a94c8144b
rCotche <roshan.cotche@gmail.com>
2024-12-20 20:21:16 +0400

    variable 1.0

--- main.gd
+++ main.gd
@@ -1,16 +1,11 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Label.text = "hello, world"
-	$Label.modulate = Color.AQUAMARINE
+var health = 100
 
 #_input function dejà present dans godot
 #event quel evenement a trigger la function
 #mouse movement ou pressed key
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("my_action"):
-		$Label.modulate = Color.RED
-	if event.is_action_released("my_action"):
-		$Label.modulate = Color.AQUAMARINE
+		health -= 20
+		print(health)
