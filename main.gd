adca0fe0f50b9427b1884e343760b63788d97710
rCotche <roshan.cotche@gmail.com>
2024-12-20 20:17:40 +0400

    input

--- main.gd
+++ main.gd
@@ -5,3 +5,12 @@ extends Node
 func _ready() -> void:
 	$Label.text = "hello, world"
 	$Label.modulate = Color.AQUAMARINE
+
+#_input function dejà present dans godot
+#event quel evenement a trigger la function
+#mouse movement ou pressed key
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("my_action"):
+		$Label.modulate = Color.RED
+	if event.is_action_released("my_action"):
+		$Label.modulate = Color.AQUAMARINE
