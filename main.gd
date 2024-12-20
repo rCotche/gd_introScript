07eb97d5ca2e96f7445d27c0a93aff0045149dfa
rCotche <roshan.cotche@gmail.com>
2024-12-20 21:27:08 +0400

    variable 2.0

--- main.gd
+++ main.gd
@@ -1,19 +1,12 @@
 extends Node
 
-var health = 100
-
-#_input function dejà present dans godot
-#event quel evenement a trigger la function
-#mouse movement ou pressed key
-func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed("my_action"):
-		#health -= 20 # un commentaire
-		#print(health)
-		#if health <= 0:
-			#health = 0
-			#print("you died")
-		#elif health < 50:
-			#print("u r injured")
-		#else:
-			#print("u r healthy")
+#var damage: int = 15 #la var "damage" de type int
+#
+#@export va nous permettre de l'utiliser dans l'inspector
+#@export var damage := 15 #godot will determine automatically le type de la var
+#
+#const var that cannot change
+#bonne pratique de mttre les const en capital
+const GRAVITY = -9.81
+func _ready() -> void:
 	pass
