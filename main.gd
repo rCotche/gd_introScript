8ff92eb4158552c78a238781f3362d273c3ad5cc
rCotche <roshan.cotche@gmail.com>
2024-12-20 20:28:47 +0400

    comments

--- main.gd
+++ main.gd
@@ -6,13 +6,14 @@ var health = 100
 #event quel evenement a trigger la function
 #mouse movement ou pressed key
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("my_action"):
-		health -= 20
-		print(health)
-		if health <= 0:
-			health = 0
-			print("you died")
-		elif health < 50:
-			print("u r injured")
-		else:
-			print("u r healthy")
+	#if event.is_action_pressed("my_action"):
+		#health -= 20 # un commentaire
+		#print(health)
+		#if health <= 0:
+			#health = 0
+			#print("you died")
+		#elif health < 50:
+			#print("u r injured")
+		#else:
+			#print("u r healthy")
+	pass
