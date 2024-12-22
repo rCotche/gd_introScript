5b45a8bed92ba4010b7b3ca76af8e095d2444643
rCotche <roshan.cotche@gmail.com>
2024-12-22 07:25:39 +0400

    random numbers

--- main.gd
+++ main.gd
@@ -1,22 +1,13 @@
 extends Node
 
-#func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed("my_action"):
-		#jump()
-#
-#func jump():
-	##add upward force
-	##play a boing song
-	##play jump animation
-	#print("jump")
-
 func _ready() -> void:
-	var result = add(3,5)
-	print(result)
-
-#typé les params = num1: int
-# return type syntaxe = -> int
-func add(num1: int, num2: int) -> int:
-	var result = num1 + num2
-	#print(result)
-	return result
+	#random numbres between 0 et 1
+	#randf()
+	#var roll = randf()
+	#if roll <= 0.8:
+		#print("common item")
+	#else:
+		#print("rare item")
+	#random intger betwenn 2 values
+	var character_height = randi_range(140, 210)
+	print("character height : " + str(character_height) + " cm.")
