31697e0b048761cd3f606b7a3bbbc3e64df77760
rCotche <roshan.cotche@gmail.com>
2024-12-22 09:20:39 +0400

    signals

--- main.gd
+++ main.gd
@@ -1,12 +1,30 @@
 extends Node
 
-#@onready make sure all node r loaded
-#$ = get_node("")
-#path relative
-#@onready var weapon: Sprite2D = $Player/Weapon
-#@export var my_node: Node
-@export var my_node: Sprite2D
+#func _ready() -> void:
+	#pass
+#
+#
+#func _on_button_pressed() -> void:
+	#print("Money")
 
+#creat a signal
+#signal level_up
+signal level_up(msg)
+
+var xp := 0
+
+#connect signal througt code
 func _ready() -> void:
-	if my_node is Node2D:
-		print("is 2d")
+	level_up.connect(_on_level_up)# pas besoin de parenthese
+	#level_up.disconnect(_on_level_up)# pas besoin de parenthese
+
+func _on_timer_timeout() -> void:
+	xp += 5
+	print(xp)
+	if xp >= 20:
+		xp = 0
+		level_up.emit("nice")# trigger le signal
+
+
+func _on_level_up(msg) -> void:
+	print(msg)
