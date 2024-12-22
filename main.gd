c7589ceb2cea89778ff3072d7e33c49061cb8021
rCotche <roshan.cotche@gmail.com>
2024-12-22 07:42:11 +0400

    loops

--- main.gd
+++ main.gd
@@ -1,13 +1,20 @@
 extends Node
 
 func _ready() -> void:
-	#var items = ["Potion", 3, 6]
-	var items: Array[String] = ["Potion", "Feather", "Stolen harp"]
-	#update an element in array
-	items[1] = "Smelly socks"
-	items[2] = "Staff"
+	##var items = ["Potion", 3, 6]
+	#var items: Array[String] = ["Potion", "Feather", "Stolen harp"]
+	##foreach equivalent
+	#for item in items:
+		#print(item)
 	
-	#delete un element à l'index
-	items.remove_at(1)
-	items.append("overpowerd sword")
-	print(items[0])
+	#n = iteration of the loop
+	#for n in 8:
+		#print(n)
+	var glass := 0.0
+	
+	while glass < 0.5:
+		glass += randf_range(0.01, 0.2)
+		print(glass)
+		if glass > 0.2:
+			break #sort de la boucle & continue skip iteration
+	print("glass half full")
